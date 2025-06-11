4cc1d3040b98f464a7521e509734c12764b4b66b
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2025-06-11 19:36:21 +0300

    production ready

--- app/api/development-status/[id]/route.ts
+++ app/api/development-status/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function PATCH(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   const data = await request.json();
