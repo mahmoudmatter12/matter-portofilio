b39796c71345a13198f193e93675ef43c7b0a2c3
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2025-07-25 23:47:17 +0300

    opt

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["example.com", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -14,8 +23,6 @@ const nextConfig: NextConfig = {
       "react-icons",
       "@radix-ui/react-icons",
     ],
-    // Reduce bundle size for mobile
-    optimizeCss: true,
   },
   turbopack: {
     rules: {
